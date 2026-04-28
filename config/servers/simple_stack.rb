e52a2952bdc3375bf4012b8edcb6b0d822f82bf4
_Loopa_ <vedernikov.ai@phystech.edu>
2026-04-28 20:14:48 +0300

    feat: log with auth bot

--- config/servers/simple_stack.rb
+++ config/servers/simple_stack.rb
@@ -1,6 +1,6 @@
 Server :simple_stack do
-  host '<change_ip>'
-  tls_domain '<change_domain>'
+  host '89.124.114.155'
+  tls_domain 'nfc-pantheon.ru'
 
   install :swarm
   install :traefik
@@ -10,17 +10,10 @@ Server :simple_stack do
   install :registry_listener
 
   env AUTH_BOT: 'eksli_auth_bot'
-  env AUTH_VERIFY_KEY: '<change_me>'
-  env MAIN_DOMAIN: '<change_domain>'
+  env AUTH_VERIFY_KEY: 'change_me'
+  env MAIN_DOMAIN: 'nfc-pantheon.ru'
   env FORWARD_OAUTH_AUTH_URL: '/oauth_slim/authorize'
   env CONSOLE_LEVEL: 'all'
 
-  env INSIGHT_BASIC_AUTH: 'admin:admin'
-
-  env USERS_YAML: '/users.yaml'
-  config target: '/users.yaml', file_content: <<~YAML
-    users:
-      - admin: { Name: Fullname, role: admin, org: eksli, enabled: true, password: admin }  
-      # - admin: { Name: Fullname, role: admin, org: eksli, enabled: true, password: admin }  
-  YAML
+  env INSIGHT_BASIC_AUTH: 'admin:password'
 end
